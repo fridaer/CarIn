66bc613c81adfbfd07f2e4547f42bc818eeb52f9
Tobias Öhrström <tobias@ohrstrom.se>
2013-05-07 16:09:03 +0200

    Lagt till Livedata från microsofts bing-api i kart demot!

--- CarIn/Scripts/MyMapboxScript.js
+++ CarIn/Scripts/MyMapboxScript.js
@@ -2,97 +2,62 @@
 layer.on('ready', function () {
     // the layer has been fully loaded now, and you can
     // call .getTileJSON and investigate its properties
+    var map = L.mapbox.map('map', 'tobohr.map-n6vjouf7').setView([57.681789894876026, 12.239759687484923], 11);
 
-    var map = L.mapbox.map('map', 'tobohr.map-n6vjouf7')
-        .setView([57.681789894876026, 12.239759687484923], 11);
-    L.mapbox.markerLayer({
-        // this feature is in the GeoJSON format: see geojson.org
-        // for the full specification
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            // coordinates here are in longitude, latitude order because
-            // x, y is the standard for GeoJSON and many formats
-            coordinates: [12.239759687484923, 57.681789894876026]
-        },
-        properties: {
-            title: 'Feeeting kön',
-            description: '2 långtradare har koliderat, brinner på vägbannan',
-            // one can customize markers by adding simplestyle properties
-            // http://mapbox.com/developers/simplestyle/
-            'marker-symbol': 'cross',
-            'marker-size': 'large',
-            'marker-color': '#3E679E'
-        }
-    }).addTo(map);
-    L.mapbox.markerLayer({
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [12.25, 57.8]
-        },
-        properties: {
-            title: 'Stora störningar i skogen i bushen.',
-            description: 'Var vaksam när du är ute och går',
-            'marker-symbol': 'cross',
-            'marker-size': 'small',
-            'marker-color': '#3E679E'
-        }
-    }).addTo(map);
-    L.mapbox.markerLayer({
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [12.032776, 57.63511]
-        },
-        properties: {
-            title: 'Stora störningar i trafiken',
-            description: 'Medelhastighet 55km/h',
-            'marker-symbol': 'cross',
-            'marker-size': 'small',
-            'marker-color': '#3E679E'
-        }
-    }).addTo(map);
-    L.mapbox.markerLayer({
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [12.008743, 57.774884]
-        },
-        properties: {
-            title: 'Stora störningar i trafiken',
-            description: 'Medelhastighet 35km/h',
-            'marker-symbol': 'cross',
-            'marker-size': 'small',
-            'marker-color': '#3E679E'
-        }
-    }).addTo(map);
-    L.mapbox.markerLayer({
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [12.098694, 57.736784]
-        },
-        properties: {
-            title: 'Stora störningar i trafiken',
-            description: 'Medelhastighet 35km/h',
-            'marker-symbol': 'cross',
-            'marker-size': 'small',
-            'marker-color': '#3E679E'
-        }
-    }).addTo(map);
-    L.mapbox.markerLayer({
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [11.990891, 57.718086]
-        },
-        properties: {
-            title: 'Spår Arbete',
-            description: 'Försvårad framkomlighet i centrala delar av stan.',
-            'marker-symbol': 'cross',
-            'marker-size': 'small',
-            'marker-color': '#3E679E'
-        }
-    }).addTo(map);
+        (function ($) {
+            var url = 'http://dev.virtualearth.net/REST/v1/Traffic/Incidents/57.497813,11.602687,57.885356,12.406062?key=AoWk0xixw7Xr16xE6Tne-3nNsYihl9ab7yIhnoASonYm2sWCdYk7VNhhAUg82cUj&o=json&jsonp=?';
+
+            $.ajax({
+                type: 'GET',
+                url: url,
+                async: true,
+                jsonpCallback: 'jsonCallback',
+                contentType: 'application/json',
+                dataType: 'jsonp',
+                success: function (json) {
+                    console.log(json);
+                    $.each(json.resourceSets[0].resources, function () {
+                        console.log(this);
+                        L.mapbox.markerLayer({
+                            type: 'Feature',
+                            geometry: {
+                                type: 'Point',
+                                coordinates: [this.point.coordinates[1], this.point.coordinates[0]]
+                            },
+                            properties: {
+                                title: this.description,
+                                description: this.description,
+                                'marker-symbol': 'cross',
+                                'marker-size': 'large',
+                                'marker-color': '#EB0000'
+                            }
+                        }).addTo(map);
+                        
+                        if (this.point.coordinates[0] !== this.toPoint.coordinates[0] || this.point.coordinates[1] !== this.toPoint.coordinates[1])
+                        {
+                            console.log(this);
+                            L.mapbox.markerLayer({
+                                type: 'Feature',
+                                geometry: {
+                                    type: 'Point',
+                                    coordinates: [this.toPoint.coordinates[1], this.toPoint.coordinates[0]]
+                                },
+                                properties: {
+                                    title: this.description,
+                                    description: this.description,
+                                    'marker-symbol': 'cross',
+                                    'marker-size': 'large',
+                                    'marker-color': '#EB0000'
+                                }
+                            }).addTo(map);
+                        }
+                    });
+
+                },
+                error: function (e) {
+                    console.log("Det gick åt apan :( ");
+                }
+            });
+
+        })(jQuery);
 })
