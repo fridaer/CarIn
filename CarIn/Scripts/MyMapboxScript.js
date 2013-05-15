de0905db76147a740f4abd4ea9586ecb37a0f766
Tobias Öhrström <tobias@ohrstrom.se>
2013-05-15 23:19:53 +0200

    Test-ikoner, Enable-disable Trafikstörningar. Måla på vägen-funktion påbörjad med hjälp av Mapquestapi (Skall göras om till serverside-kod)

--- CarIn/Scripts/MyMapboxScript.js
+++ CarIn/Scripts/MyMapboxScript.js
@@ -1,23 +1,23 @@
 ﻿/// <reference path="mapbox.js" />
-var layer = L.mapbox.tileLayer('tobohr.map-n6vjouf7');
-layer.on('ready', function () {
-    // the layer has been fully loaded now, and you can
-    // call .getTileJSON and investigate its properties
-    var map = L.mapbox.map('map', 'tobohr.map-n6vjouf7').setView([57.75, 11.974749], 11);
-   
-    var updateEvery2sec = setInterval(function () {
-        var zoomlevel = map.getZoom();
-        if (zoomlevel <= 11) {
-            map.setView([57.75, 11.974749], 11);
-            map.dragging.disable();
-        }
-        else {
-            map.dragging.enable();
-        }
-    },500);
+$(document).ready(function () {
 
-        (function ($) {
-            //var url = 'http://dev.virtualearth.net/REST/v1/Traffic/Incidents/57.497813,11.602687,57.885356,12.406062?key=AoWk0xixw7Xr16xE6Tne-3nNsYihl9ab7yIhnoASonYm2sWCdYk7VNhhAUg82cUj&o=json&jsonp=?';
+    var layer = L.mapbox.tileLayer('tobohr.map-n6vjouf7');
+    layer.on('ready', function () {
+        // the layer has been fully loaded now, and you can
+        // call .getTileJSON and investigate its properties
+        var map = L.mapbox.map('map', 'tobohr.map-n6vjouf7').setView([57.75, 11.974749], 11);
+
+        var updateEvery2sec = setInterval(function () {
+            var zoomlevel = map.getZoom();
+            if (zoomlevel <= 11) {
+                map.setView([57.75, 11.974749], 11);
+                map.dragging.disable();
+            }
+            else {
+                map.dragging.enable();
+            }
+        }, 500);
+    
 
             var url = "/api/v1/CarInRESTful/GetAllInfo/";
             $.ajax({
@@ -50,48 +50,63 @@ layer.on('ready', function () {
                             //WindCode: "S"
                             //WindSpeedMps: "6.8"
                     console.log(json);
+                    var id = 0; 
                     $.each(json.TrafficIncidents, function () {
                         console.log(this);
-                        L.mapbox.markerLayer({
-                            type: 'Feature',
-                            geometry: {
-                                type: 'Point',
-                                coordinates: [this.PointLong, this.PointLat]
-                            },
-                            properties: {
-                                title: this.Description,
-                                description: this.Description,
-                                'marker-symbol': 'cross',
-                                'marker-size': 'large',
-                                'marker-color': '#EB0000'
-                            }
-                        }).addTo(map);
-                        
-                        if (this.PointLong !== this.ToPointLong || this.PointLat !== this.ToPointLat)
-                        {
-                            console.log(this);
-                            L.mapbox.markerLayer({
-                                type: 'Feature',
-                                geometry: {
-                                    type: 'Point',
-                                    coordinates: [this.ToPointLong, this.ToPointLat]
-                                },
-                                properties: {
-                                    title: this.Description,
-                                    description: this.Description,
-                                    'marker-symbol': 'cross',
-                                    'marker-size': 'large',
-                                    'marker-color': '#EB0000'
-                                }
-                            }).addTo(map);
-                        }
+                        id++;
+
+                        var myIcon = L.divIcon({ className: 'traffic-problem icon-attention'});
+                        var popupContent =  '<p id="'+ id +'">'+ this.Description + '</p>';
+                        var themarker = L.marker([this.PointLat, this.PointLong], { icon: myIcon }).addTo(map).bindPopup(popupContent);
                     });
+
                     $('.vader span').text(json.WheatherPeriods[0].TemperatureCelsius + "\u2103");
+                },
+                error: function (e) {
+                    console.log("Det gick åt apan :( ");
+                }
+    });
+
+            $("#olyckaKnapp").click(function () {
+                $(".traffic-problem").toggle();
+            });
+            var i = 0;
+            var points = new Array();
+            var longlat = new Array();
+            var url2 = "http://www.mapquestapi.com/directions/v1/route?key=Fmjtd%7Cluub2du8n1%2C2g%3Do5-9u2xg4&=renderAdvancedNarrative&ambiguities=ignore&avoidTimedConditions=false&doReverseGeocode=false&outFormat=json&routeType=fastest&timeType=0&enhancedNarrative=false&shapeFormat=raw&generalize=1&locale=sv_SE&unit=k&from=57.69985,%2011.96114&to=%2057.70015,%2011.95216&drivingStyle=2&highwayEfficiency=21.0";
+            $.ajax({
+                type: 'GET',
+                url: url2,
+                async: true,
+                dataType: 'jsonp',
+                success: function (jsonPolyline) {
+                    $.each(jsonPolyline.route.shape.shapePoints, function () {
+                        var temp = this.toString();
+                        
+                        var onepoint = parseFloat(temp);
+                        points.push(onepoint);
+                        console.log(onepoint);
+
+                        if (i % 2 ==! 0) {
+                            longlat.push(points);
+                            points = [];
+                        }
+                        i++;
+                    })
+
+                    console.log(longlat);
+
+                    var polyline_options = {
+                        color: '#000'
+                    };
+
+                    var polyline = L.polyline(longlat, polyline_options).addTo(map);
+
                 },
                 error: function (e) {
                     console.log("Det gick åt apan :( ");
                 }
             });
 
-        })(jQuery);
+    });
 })
