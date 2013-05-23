239b7dad2215a491322b06282cc50ff9bb4aa89e
Björn Strömblom <bjorn.stromblom@gmail.com>
2013-05-23 14:47:13 +0200

    Fixat navigationspilarna

--- CarIn/Scripts/scripts.js
+++ CarIn/Scripts/scripts.js
@@ -2,10 +2,16 @@
     if (Modernizr.mq('only all and (max-width: 767px)')) { //480px
         //var viewportHeight = $(window).height();
         var navMenuHight = $("nav.mainmenu").height();
-        console.log(navMenuHight);
         var deviceHight = $('#map').height($(window).height());
         var mapHight = deviceHight - navMenuHight;
         $('#map').height(mapHight);
+        window.setTimeout(moveMapNavArrows, 100)
+        $('.leaflet-left').removeClass('leaflet-left').addClass('leaflet-right');
+
+
+        function moveMapNavArrows (){
+            $('.leaflet-left').removeClass('leaflet-left').addClass('leaflet-right');
+        }
 
     }
 
