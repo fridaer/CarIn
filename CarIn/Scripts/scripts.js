121ec5c1c9025801c31f1feb5c5c60cf86055847
Björn Strömblom <bjorn.stromblom@gmail.com>
2013-05-29 15:56:01 +0200

    Lagt till iOS grejer

--- CarIn/Scripts/scripts.js
+++ CarIn/Scripts/scripts.js
@@ -1,6 +1,17 @@
 ﻿$(document).ready(function () {
-
-   if (Modernizr.mq('only all and (max-width: 767px)')) { //480px
+   
+    if (Modernizr.mq('only all and (max-width: 767px)')) { //480px
+        if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || true) {
+            if (!window.navigator.standalone){
+                window.location.replace("home/iOS_Install");
+                //$("#container").remove();
+                //$.get('home/iOS_Install', function (data) {
+                //    $('body').html(data);
+                //    alert('Load was performed.');
+                //});
+            }
+        }
+        
         //var viewportHeight = $(window).height();
         $("nav.mainmenu img").imagesLoaded(function(){
             var navMenuHight = $("nav.mainmenu").height();
@@ -10,15 +21,27 @@
             var mapHight = deviceHight - navMenuHight;
             console.log("mapHight = " + mapHight);
             $('#map').height(mapHight);
+            
         })
 
     }
     window.setTimeout(moveMapNavArrows, 300)
     function moveMapNavArrows() {
         $('.leaflet-left').removeClass('leaflet-left').addClass('leaflet-right');
+        $('div.leaflet-bottom.leaflet-right').remove();
     }
 
     if (window.navigator.standalon) {
         alert("window.navigator.standalon = true");
     }
 });
+
+
+
+function ShowLoadingDiv() {
+    $("#appStartLoading").slideDown("slow");
+}
+
+function HideLoadingDiv() {
+    $("#appStartLoading").slideUp("slow");
+}
